f8684040fbbf94a2f2c4e3376a8e2eca064532e5
TheTuso <piotrekpasztor@gmail.com>
2023-04-17 19:08:14 +0200

    Add LuckPerms API

--- src/main/kotlin/pl/tuso/switcher/Switcher.kt
+++ src/main/kotlin/pl/tuso/switcher/Switcher.kt
@@ -5,6 +5,8 @@ import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.proxy.ProxyServer
+import net.luckperms.api.LuckPerms
+import net.luckperms.api.LuckPermsProvider
 import org.slf4j.Logger
 
 @Plugin(
@@ -12,15 +14,17 @@ import org.slf4j.Logger
     name = "Switcher",
     version = "1.0",
     url = "https://github.com/TheTuso/Switcher",
-    description = "A Simple Velocity plugin that allows players to join from multiple game profiles using a domain",
+    description = "A simple Velocity plugin that allows players to join from multiple game profiles using a domain",
     authors = ["tuso"])
 class Switcher @Inject constructor(val proxyServer: ProxyServer, val logger: Logger) {
+    lateinit var luckPerms: LuckPerms
+
     init {
         logger.info("Heyo! Enabling Switcher...")
     }
 
     @Subscribe
     fun onProxyInitialization(initializeEvent: ProxyInitializeEvent) {
-
+        if (proxyServer.pluginManager.isLoaded("luckperms")) luckPerms = LuckPermsProvider.get()
     }
 }
