b62435c657f9b05354c8c563b5fd17f49f5f6cbc
TheTuso <piotrekpasztor@gmail.com>
2023-04-25 19:12:35 +0200

    Add comments and change the messages

--- src/main/kotlin/pl/tuso/switcher/SwitchRequestListener.kt
+++ src/main/kotlin/pl/tuso/switcher/SwitchRequestListener.kt
@@ -18,25 +18,29 @@ class SwitchRequestListener(private val switcher: Switcher) {
         val virtualHost = InetSocketAddress::getHostString.let { requestEvent.connection.virtualHost.map(it).orElse("").lowercase() }
         val username = getUsernameFromHost(virtualHost) ?: return
 
+        // Checks if player is trying to connect via switcher without changing the username
         if (username == requestEvent.username) {
-            switcher.logger.warn("${requestEvent.username} tried to connect through the switcher with the same username!")
+            switcher.logger.warn("${requestEvent.username} tried to connect via switcher without changing the username!")
             return
         }
 
+        // Checks if the username complies with minecraft username standards
         if (!"^\\w{3,16}$".toRegex().matches(username)) {
-            switcher.logger.warn("${requestEvent.username} tried to connect through the switcher with invalid username!")
+            switcher.logger.warn("${requestEvent.username} tried to connect via switcher with invalid username!")
             return
         }
 
+        // Checks player permission
         if (!switcher.luckPerms.userManager.loadUser(requestEvent.gameProfile.id).get().
             cachedData.permissionData.checkPermission("switcher.domain").asBoolean()) return
 
+        // Sets a new gameProfile for the connecting player
         requestEvent.gameProfile = if (requestEvent.isOnlineMode) {
             val userJsonObject = getUserJsonObject(username.lowercase())
             val onlineUniqueId = userJsonObject?.get("id")?.asString
             val onlineUsername = userJsonObject?.get("name")?.asString
 
-            val gameProfile: GameProfile = if (onlineUniqueId != null && onlineUsername != null)
+            val gameProfile = if (onlineUniqueId != null && onlineUsername != null)
                 GameProfile(onlineUniqueId, onlineUsername, listOf())
             else
                 GameProfile.forOfflinePlayer(username)
@@ -47,10 +51,12 @@ class SwitchRequestListener(private val switcher: Switcher) {
         switcher.logger.info("${requestEvent.username} is logging as $username")
     }
 
+    /** Returns username from the given [host] */
     private fun getUsernameFromHost(host: String): String? = "(?<=$prefix)[^.]+".toRegex()
         .matchAt(host, host.indexOf(prefix) + prefix.length)
         ?.value
 
+    /** Returns [JsonObject] for the user with the given [username] */
     private fun getUserJsonObject(username: String): JsonObject? {
         val response = try {
             URL("https://api.mojang.com/users/profiles/minecraft/$username").readText()
